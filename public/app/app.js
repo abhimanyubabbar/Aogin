d28d0f950fb75fe5cd295f4191d9d3ed902c4904
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-05-30 22:51:11 +0200

    Updated the login page with more bootstrap.

--- public/app/app.js
+++ public/app/app.js
@@ -6,13 +6,8 @@
 
     app.config(['$logProvider','$routeProvider', function($logProvider, $routeProvider){
 
-
         $logProvider.debugEnabled(true);
 
-        $routeProvider
-            .when("/", {
-
-            })
     }]);
 
 })();
