92af0d8c5ad42dbdc12872d35f75612d8de54eec
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-06-02 23:49:34 +0200

    Minor Upgrades.

--- public/app/controller/loginController.js
+++ public/app/controller/loginController.js
@@ -12,8 +12,17 @@
         $log.info("Login Controller Initialized");
 
         var vm = this;
+
+        vm.data ={
+            username: '',
+            password:''
+        };
+
         vm.submit = function(){
             $log.debug("Form Submit Called");
+            $log.debug(vm.data);
+
+            $log.debug(JSON.stringify(vm.data));
         }
     }
 
